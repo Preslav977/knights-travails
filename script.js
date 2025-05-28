c721a91aec988a399caf91aee2564e4f76c34475
Preslav977 <p.cvetanow@gmail.com>
2025-05-28 12:54:05 +0300

    Figured out that I was supposed to swap x,y where the calculation was happening of the moves, that caused bug to get the same moves twice

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function allPossibleMoves([x, y]) {
-  return (
+  return [
     [x - 2, y - 1],
     [x - 2, y + 1],
     [x + 2, y - 1],
     [x + 2, y + 1],
-    [y - 2, x - 1],
-    [y - 2, x + 1],
-    [y + 2, x - 1],
-    [y + 2, x + 1]
-  );
+    [x - 1, y - 2],
+    [x + 1, y - 2],
+    [x - 1, y + 2],
+    [x + 1, y + 2],
+  ];
 }
 
-// console.log(allPossibleMoves([0, 0]));
+console.log(allPossibleMoves([0, 0]));
 
 function checkIfNotOutOfBounds([x, y]) {
   if (x < 0 || y > 7 || y < 0 || x > 7) {
@@ -26,9 +26,11 @@ function checkIfNotOutOfBounds([x, y]) {
 function knightMoves([x, y], [s, e]) {
   const queue = [];
 
-  const knightMovements = allPossibleMoves([x, y]);
+  const visitedNodes = [];
 
-  queue.push(knightMovements);
+  let knightMovements = allPossibleMoves([x, y]);
+
+  // console.log(knightMovements);
 
   const checkIfStartPathIsNotOutOfBounds = checkIfNotOutOfBounds([x, y]);
 
@@ -37,12 +39,20 @@ function knightMoves([x, y], [s, e]) {
   if (checkIfStartPathIsNotOutOfBounds || checkIfEndPathIsNotOutOfBounds) {
     throw new Error("Knight is out of bounds");
   } else {
-    while (queue.length !== 0) {
-      queue.forEach((element) => {
-        console.log(element);
-      });
+    for (let i = 0; i < knightMovements.length; i++) {
+      // console.log(knightMovements[i]);
+
+      const moveIsNotOutOfBound = checkIfNotOutOfBounds(knightMovements[i]);
+
+      if (!moveIsNotOutOfBound) {
+        queue.push([knightMovements[i], [x, y]]);
+
+        // console.log(queue);
+      }
     }
+
+    // while (queue.length !== 0) {}
   }
 }
 
-console.log(knightMoves([3, 3], [7, 2]));
+console.log(knightMoves([0, 0], [3, 3]));
